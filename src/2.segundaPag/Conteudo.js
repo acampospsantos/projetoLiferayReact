d0f38cc05913fcb646c9263de24f331b9e50617b
antsu comuna <acampospsantos@hotmail.com>
2022-06-02 11:41:02 -0300

    Commit - Segunda Página 2.0

---
+++ src/2.segundaPag/Conteudo.js
@@ -0,0 +1,26 @@
+
+import './Conteudo.css';
+
+function MiddleContent() {
+    return (
+        <div className="conteudo">
+            <div className="centerConteudo">
+
+                <div className="box-botoes">
+                    <div className="botoes">
+                        <ul>
+                            <li> <a href="#"> Início </a> </li>
+                            <li> <a href="#"> Voluntários </a> </li>
+                            <li> <a href="#"> Organizações </a> </li>
+                            <li> <a href="#"> Entrar </a> </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default MiddleContent;
