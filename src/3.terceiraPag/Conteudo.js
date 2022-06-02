ef494b45e2842d654a1b68cba200590cda5acbaf
antsu comuna <acampospsantos@hotmail.com>
2022-06-02 12:04:05 -0300

    Commit - Terceira Página 2.0

---
+++ src/3.terceiraPag/Conteudo.js
@@ -0,0 +1,29 @@
+
+import './Conteudo.css';
+
+function MiddleContent() {
+    return (
+        <div className="conteudo">
+            <div className="centerConteudo">
+
+                <div className="box-botoes">
+                    <div className="botoes">
+                        <ul>
+                            <li> <a href="#"> Ações Inscritas </a> </li>
+                            <li> <a href="#"> Vagas Disponíveis </a> </li>
+                            <li> <a href="#"> Sair </a> </li>                            
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="portalVoluntario">
+                    <h1> Portal Voluntário </h1>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default MiddleContent;
