78f5ec1f97355c104bae6b3c4c71e7785ea8d6da
antsu comuna <acampospsantos@hotmail.com>
2022-06-02 12:18:16 -0300

    Commit - Quarta Página 2.0

---
+++ src/4.quartaPag/Conteudo.js
@@ -0,0 +1,29 @@
+
+import './Conteudo.css';
+
+function MiddleContent() {
+    return (
+        <div className="conteudo">
+            <div className="centerConteudo">
+
+                <div className="box-botoes">
+                    <div className="botoes">
+                        <ul>
+                            <li> <a href="#"> Ações promovidas </a> </li>
+                            <li> <a href="#"> Criar nova ação </a> </li>
+                            <li> <a href="#"> Sair </a> </li>                            
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="portalOrganização">
+                    <h1> Portal Organização </h1>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default MiddleContent;
