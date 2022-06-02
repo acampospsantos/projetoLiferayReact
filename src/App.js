e7d232614818ec8071f84230c6600d95daeb5c58
antsu comuna <acampospsantos@hotmail.com>
2022-06-02 10:49:47 -0300

    Commit - Primeira Página

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Cabeçalho';
+import MiddleContent from './Conteudo';
+import Footer from './Rodapé';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header /> {/* Cabeçalho */}
+
+      <MiddleContent /> {/* Conteúdo do meio da página */}
+
+      <Footer /> {/* Rodapé */}      
     </div>
   );
 }
---
+++ src/Conteudo.js
@@ -0,0 +1,15 @@
+
+import './Conteudo.css';
+
+function MiddleContent(){
+    return (
+        <div className="conteudo">
+            <div className="center">
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default MiddleContent;
