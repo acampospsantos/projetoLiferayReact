b8e9304e0af9044290455d9fd107eeededa0c5c9
antsu comuna <acampospsantos@hotmail.com>
2022-06-02 12:01:03 -0300

    Commit - Terceira Página

--- src/Conteudo.js
+++ src/Conteudo.js
@@ -9,12 +9,15 @@ function MiddleContent() {
                 <div className="box-botoes">
                     <div className="botoes">
                         <ul>
-                            <li> <a href="#"> Início </a> </li>
-                            <li> <a href="#"> Voluntários </a> </li>
-                            <li> <a href="#"> Organizações </a> </li>
-                            <li> <a href="#"> Entrar </a> </li>
+                            <li> <a href="#"> Ações Inscritas </a> </li>
+                            <li> <a href="#"> Vagas Disponíveis </a> </li>
+                            <li> <a href="#"> Sair </a> </li>                            
                         </ul>
                     </div>
+                </div>
+
+                <div className="portalVoluntario">
+                    <h1> Portal Voluntário </h1>
                 </div>
 
             </div>
