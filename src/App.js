1dd4996a5a811764cca43b145cdc7ab3657821f7
antsu comuna <acampospsantos@hotmail.com>
2022-06-05 17:59:54 -0300

    Commit - Página SETE (Título)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
-import Header from './2.segundaPagPadrao/6ºPágina/Cabeçalho';
-import MiddleContent from './2.segundaPagPadrao/6ºPágina/Conteudo';
-import Footer from './2.segundaPagPadrao/6ºPágina/Rodapé';
+import Header from './2.segundaPagPadrao/7ºPágina/Cabeçalho';
+import MiddleContent from './2.segundaPagPadrao/7ºPágina/Conteudo';
+import Footer from './2.segundaPagPadrao/7ºPágina/Rodapé';
 
 
 function App() {
