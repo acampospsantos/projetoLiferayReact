707144ddcaf48fe9726c7b002afa2b0ea5af7935
antsu comuna <acampospsantos@hotmail.com>
2022-06-08 12:51:17 -0300

    Commit - Página CINCO - Update RASCUNHO

--- src/2.segundaPagPadrao/5ºPágina/Conteudo.js
+++ src/2.segundaPagPadrao/5ºPágina/Conteudo.js
@@ -21,6 +21,29 @@ function MiddleContent() {
                     <h1> CADASTRO VOLUNTÁRIO </h1>
                 </div>
 
+                <div className="form">
+                    <form>
+                        <div className="items-form">                            
+                            <input placeholder="CPF..." type="text" />  
+                            <h4> CPF </h4>                          
+                            <input placeholder="Nome..." type="text" />
+                            <h4> NOME </h4>                            
+                            <input placeholder="Email..." type="text" />
+                            <h4> EMAIL </h4>                            
+                            <input placeholder="Telefone..." type="text" />
+                            <h4> TELEFONE </h4>                            
+                            <input placeholder="Gênero..." type="text" />
+                            <h4> GÊNERO </h4>                            
+                            <input placeholder="Senha..." type="text" />
+                            <h4> SENHA </h4>                            
+                            <input placeholder="Confirmar..." type="text" />
+                            <h4> CONFIRMAR </h4>
+                            <p> Use oito ou mais caracteres com uma combinação de letras, números e símbolos </p>
+                        </div>                        
+                        <input type="submit" />                        
+                    </form>
+                </div>
+
             </div>
 
         </div>
