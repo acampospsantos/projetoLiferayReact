7144becf428075acdcd5e6253da1f70b92dc468c
antsu comuna <acampospsantos@hotmail.com>
2022-06-08 11:12:37 -0300

    Commit - Página DOIS - Update

--- src/2.segundaPagPadrao/2ºPágina/Conteudo.js
+++ src/2.segundaPagPadrao/2ºPágina/Conteudo.js
@@ -21,13 +21,20 @@ function MiddleContent() {
                     <h1> VOLUNTÁRIOS </h1>
                 </div>
 
-                <div className="textoPag2">
-                    <h3> O voluntariado é sobre trocas - de experiência, conhecimento, serviço, apoio, cidadania, carinho, amor... Existindo um ganho não apenas para a organização, mas também para o voluntário. É uma experiência para todos independe de gênero, raça ou etnia que permite conhecer novas realidades, se engajar em uma causa social, contribuir com suas habilidades, e até mesmo desenvolver novas competências. </h3>
+                <div className="meiota">
+                    <div className="textoPag2">
+                        <h3> O voluntariado é sobre trocas - de experiência, conhecimento, serviço, apoio, cidadania, carinho, amor... Existindo um ganho não apenas para a organização, mas também para o voluntário. É uma experiência para todos independe de gênero, raça ou etnia que permite conhecer novas realidades, se engajar em uma causa social, contribuir com suas habilidades, e até mesmo desenvolver novas competências. </h3>
+                    </div>
+
+                    <div className="vemSerVoluntario">
+                        <a href=".\2.segundaPagPadrao\5ºPágina\Pag5.js"> <h3> Vem ser um voluntário </h3> </a>
+                    </div>
+
+                    <div className="blueImage">
+                        <img src="https://www.liferay.com/documents/10182/0/LXC+Diagram+for+Liferay.com.svg/6f4ed038-b8ed-ed20-319d-1c248aa3d2dc/LXC+Diagram+for+Liferay.com.svg?t=1647014383907&download=true" />
+                    </div>
                 </div>
 
-                <div className="vemSerVoluntario">
-                    <a href='#'> <h3> Vem ser um voluntário </h3> </a>
-                </div>
 
             </div>
 
