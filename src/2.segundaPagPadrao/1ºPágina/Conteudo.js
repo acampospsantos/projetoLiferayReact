cc68bfa9dd0c66e24f5e7654d5a378d7d7dd078c
antsu comuna <acampospsantos@hotmail.com>
2022-06-08 11:25:21 -0300

    Commit - Página UM - Update

--- src/2.segundaPagPadrao/1ºPágina/Conteudo.js
+++ src/2.segundaPagPadrao/1ºPágina/Conteudo.js
@@ -21,6 +21,22 @@ function MiddleContent() {
                     <h1> SISTEMA DE VOLUNTARIADO LIFERAY </h1>
                 </div>
 
+                <div className="meiota">
+
+                    <div className="textoPag1">
+                        <h3> A Liferay Inc. foi formada por alguns engenheiros de software, uma mãe e um futuro advogado em um ambiente cercado por móveis doados por uma igreja local. Eles tinham um objetivo simples: criar tecnologia útil que beneficiasse clientes, parceiros e funcionários, e construir uma organização que ajudasse outras pessoas a alcançar seu máximo potencial. Vamos fazer a diferença juntos? </h3>
+                    </div>
+
+                    <div className="participeTambem">
+                        <a href="#"> <h3> Participe, também! </h3> </a>
+                    </div>
+
+                    <div className="peopleImage">
+                        <img src="https://www-cdn.liferay.com/cdn-cgi/image/format=contain,format=auto,metadata=none,onerror=redirect,width=1280/documents/10182/306482941/About+Us+Hero+Image+Draft1-0.png/d3163603-aa50-853b-a9a2-23a101c4a356/About+Us+Hero+Image+Draft1-0.png?t=1609363790840&download=true" />
+                    </div>
+
+                </div>
+
             </div>
 
         </div>
